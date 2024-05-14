93aa4dd461bbd97b9cc039db54e398896955ebc8
yangsejin <winter_win10@naver.com>
2024-05-14 14:26:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>리액트 앱의 구조를 확인합니다.<br/>
+          App.js가 자동으로 렌더링됩니다.</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
